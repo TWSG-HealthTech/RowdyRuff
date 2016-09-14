67f32bf7e7154d791866493f97d00fbf1b96dd1c
Shuwei <sfang@thoughtworks.com>
2016-09-14 22:21:33 +0800

    Add API to get prescription for patient (WIP)

---
+++ RowdyRuff/Areas/Medication/Controllers/API/MedicationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RowdyRuff.Areas.Medication.DTO;
+using RowdyRuff.Core.Common;
+
+namespace RowdyRuff.Areas.Medication.Controllers.API
+{
+    [Area("Medication")]
+    [Route("/medication/api/{patientUuid}")]
+    [Produces("application/json")]
+    public class MedicationController : Controller
+    {
+
+        [HttpGet]
+        [Produces(typeof(IEnumerable<GetPrescriptionDTO>))]
+        public IActionResult Index(string profileId)
+        {
+            return Json(new GetPrescriptionDTO
+            {
+                DrugOrders = new List<string>()
+                {
+                    "abc"
+                }
+            });
+        }
+    }
+}
---
+++ RowdyRuff/Areas/Medication/DTO/GetPrescriptionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace RowdyRuff.Areas.Medication.DTO
+{
+    public class GetPrescriptionDTO
+    {
+        public List<String> DrugOrders { get; set; }
+    }
+}
